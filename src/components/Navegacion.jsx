522a47305f1ca04c83d1ae21d09e31aead72ab6c
Alexis Torres Climente <220240@fppiramide.com>
2025-01-20 12:39:04 +0100

    Cambio Navegacion

--- src/components/Navegacion.jsx
+++ src/components/Navegacion.jsx
@@ -7,11 +7,20 @@ export default function Nav() {
 
     return ( 
         <nav>
-            <button onClick={() => navigate("/añadir")}>Añadir café</button>
-            <button onClick={() => navigate("/registro")}>Registrarse</button>
-            <button onClick={() => navigate("/login")}>Iniciar sesión</button>
-            <button onClick={() => navigate("/cafes")}>Lista de cafés</button>
-            <button onClick={() => navigate("/votaciones")}>Votaciones</button>
+            <div className="contenido-nav">
+                <div className="logo">
+                    <img src="/cafe-logo.svg" alt="Coffee Logo" className="shake-image"  />
+                    <h1>Cafes Piramide</h1>
+                </div>
+                <div className="botones">
+                    <button onClick={() => navigate("/añadir")}>Añadir café</button>
+                    <button onClick={() => navigate("/registro")}>Registrarse</button>
+                    <button onClick={() => navigate("/login")}>Iniciar sesión</button>
+                    <button onClick={() => navigate("/cafes")}>Lista de cafés</button>
+                    <button onClick={() => navigate("/votaciones")}>Votaciones</button>
+                </div>
+            </div>
+            <div className="embellezedor"></div>
         </nav>
     );
 }
